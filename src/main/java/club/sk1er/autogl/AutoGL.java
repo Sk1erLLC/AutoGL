758bc90713d92b6710ce1322ef73ac798c87de1c
MG95 <66237187+TheMG95@users.noreply.github.com>
2021-02-15 20:59:30 +0300

    Update to original.

--- src/main/java/club/sk1er/autogl/AutoGL.java
+++ src/main/java/club/sk1er/autogl/AutoGL.java
@@ -10,7 +10,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "autogl", name = "AutoGL", version = "1.0")
+@Mod(modid = "autogl", name = "AutoGL", version = "1.1")
 public class AutoGL {
 
     private AutoGLConfig autoGLConfig;
